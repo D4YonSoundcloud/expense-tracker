38600b5c4a7500f0cd1aef0cfabc1dab85b9e135
D4YonSoundcloud <mattd4y@gmail.com>
2020-02-20 16:05:53 -0500

    expense-tracker 4:05pm 2-20-20

---
+++ script.js
@@ -0,0 +1,64 @@
+const balance = document.getElementById("balance");
+const moneyPlus = document.getElementById("money-plus");
+const moneyMinus = document.getElementById("money-minus");
+const list = document.getElementById("list");
+const form = document.getElementById("form");
+const text = document.getElementById("text");
+const amount = document.getElementById("amount");
+
+const dummyTransactions = [
+  { id: 1, text: "Flower", amount: -20 },
+  { id: 2, text: "salary", amount: 300 },
+  { id: 3, text: "Book", amount: -10 },
+  { id: 4, text: "dude", amount: 150 }
+];
+
+let transactions = dummyTransactions;
+
+//Add transactions to DOM list
+addTransactionDom = transaction => {
+  //Get sign
+  const sign = transaction.amount < 0 ? "-" : "+";
+
+  const item = document.createElement("li");
+  // add class based on value
+  item.classList.add(transaction.amount < 0 ? "minus" : "plus");
+
+  item.innerHTML = `
+  ${transaction.text} 
+  <span>${sign}${Math.abs(transaction.amount)}</span>
+  <button class="delete-btn">x</button>
+  `;
+
+  list.appendChild(item);
+};
+
+//Update the balance, income, and expense
+updateValues = () => {
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (
+    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
+  balance.innerText = `$${total}`;
+  moneyPlus.innerText = `$${income}`;
+  moneyMinus.innerText = `$${expense}`;
+};
+
+//init applicaition
+init = () => {
+  list.innerHTML = "";
+  transactions.forEach(addTransactionDom);
+  updateValues();
+};
+
+init();
